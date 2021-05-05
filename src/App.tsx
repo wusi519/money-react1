2d1643c299446a55d77afb1a68b9135acbe3c0ce
Administrator <327742525@qq.com>
2021-05-06 01:43:26 +0800

    完成首页基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
 import {
- HashRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom';
+import 'index.scss';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+border:1px solid red;
+height:100vh;
+display:flex;
+flex-direction: column;
+`;
+const Main = styled.div`
+flex-grow:1;
+overflow: auto;
+`;
+const Nav = styled.div`
+line-height:24px;
+box-shadow: 0 0 3px rgba(0,0,0,0.25);
+>ul{
+  display:flex;
+  >li{
+    width:33.3333%;
+    text-align:center;
+    padding:16px;
+  }
+}
+`;
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
           <ul>
             <li>
               <Link to="/tags">标签页</Link>
@@ -24,24 +65,10 @@ function App() {
               <Link to="/statistics">统计页</Link>
             </li>
           </ul>
-        </nav>
+        </Nav>
 
-        <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Redirect exact from="/" to="/money"/>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
+
+      </Wrapper>
     </Router>
   );
 }
@@ -57,6 +84,7 @@ function Money() {
 function Statistics() {
   return <h2>统计</h2>;
 }
+
 function NoMatch() {
   return <h2>你输错地址了,请重新输入正确的地址</h2>;
 }
