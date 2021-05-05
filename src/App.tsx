48817ae330da10432e8b452a950d73e68af13073
Administrator <327742525@qq.com>
2021-05-06 02:41:34 +0800

    创建views目录

--- src/App.tsx
+++ src/App.tsx
@@ -6,61 +6,37 @@ import {
   Redirect,
 } from 'react-router-dom';
 import 'index.scss';
-import styled from 'styled-components';
-import Nav from './components/Nav';
 
-const Wrapper = styled.div`
-height:100vh;
-display:flex;
-flex-direction: column;
-`;
-const Main = styled.div`
-flex-grow:1;
-overflow: auto;
-`;
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
-function Tags() {
-  return <h2>标签</h2>;
-}
 
-function Money() {
-  return <h2>记账2</h2>;
-}
 
-function Statistics() {
-  return <h2>统计</h2>;
-}
 
-function NoMatch() {
-  return <h2>你输错地址了,请重新输入正确的地址</h2>;
-}
 
 export default App;
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function NoMatch() {
+  return <h2>你输错地址了,请重新输入正确的地址</h2>;
+}
+export default NoMatch
