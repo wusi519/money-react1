a719f5c720375c3f24a72859a63df5956b42efbe
Administrator <327742525@qq.com>
2021-05-06 05:37:24 +0800

    数组面板UI搞定

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
+import 'helper.scss';
 
 const TagsSection = styled.section`
 background:#fff;
@@ -70,7 +71,61 @@ font-size:24px;
     }
 `;
 const NumberPadSection = styled.section`
-
+display:flex;
+flex-direction: column;
+>.output{
+  background: #ffffff;
+  font-size:36px;
+  line-height:72px;
+  text-align: right;
+  padding:0 16px;
+  box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+   inset 0 5px 5px -5px rgba(0,0,0,0.25);
+}
+>.pad{
+  >button{
+  border:none;
+  font-size:18px;
+  float:left;
+  width:25%;
+  height:64px;
+    &.ok{
+    height:128px;
+    float:right;
+    }
+    &.zero{
+    width:50%;
+    }
+    &:nth-child(1){
+    background: #f2f2f2;
+    }
+    &:nth-child(2),
+    &:nth-child(5){
+    background:#e0e0e0;
+    }
+    &:nth-child(3),
+    &:nth-child(6),
+    &:nth-child(9){
+    background:#d3d3d3;
+    }
+    &:nth-child(4),
+    &:nth-child(7),
+    &:nth-child(10){
+    background:#c1c1c1;
+    }
+    &:nth-child(8),
+    &:nth-child(11),
+    &:nth-child(13){
+    background:#b8b8b8;
+    }
+    &:nth-child(12){
+    background:#9a9a9a;
+    }
+    &:nth-child(14){
+    background:#a9a9a9;
+    }
+  }
+}
 `;
 
 
@@ -99,8 +154,8 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -112,8 +167,8 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
