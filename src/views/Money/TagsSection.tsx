afeabf3d1c7e54132fa1841c0ba549e3a0e629f1
Administrator <327742525@qq.com>
2021-05-11 09:59:40 +0800

    创建createId,让Id自增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from '../../useTags';
+import {createId} from '../../lib/createId';
 
 
 const Wrapper = styled.section`
@@ -46,7 +47,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('请输入新的标签名');
     if (tagName !== null) {
-      setTags([...tags,{id:Math.random(),name:tagName}]);
+      setTags([...tags,{id:createId(),name:tagName}]);
     }
   };
   const onToggleTag = (tagId: number) => {
